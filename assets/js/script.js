e42448acf5177ad814f9eeddc3a58d8e5631ed36
AryanSwami <swamiaryan611@gmail.com>
2023-12-11 20:06:21 +0530

    product minimal

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,7 @@ for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
     mobileMenu[i].classList.remove("active");
     overlay.classList.remove("active");
   };
-  
+
   mobileMenuOpenBtn[i].addEventListener("click", function () {
     mobileMenu[i].classList.add("active");
     overlay.classList.add("active");
@@ -49,6 +49,8 @@ for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
   mobileMenuCloseBtn[i].addEventListener("click", mobileMenuCloseFunc);
   overlay.addEventListener("click", mobileMenuCloseFunc);
 }
+
+
 
 //accordian variable
 const accordionBtn = document.querySelectorAll("[data-accordion-btn]");
